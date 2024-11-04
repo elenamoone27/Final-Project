8e53246fb682528f361912fbd0b001a82ab33cdd
Elena Moore <exm230000@utdallas.edu>
2024-11-04 17:54:06 -0600

    imported turtle and set up main function

--- src/ufosandstars.py
+++ src/ufosandstars.py
@@ -1,1 +1,15 @@
 # Create black background with scattered stars
+import turtle
+
+def main():
+
+    print("main function")
+
+def create_twinkling_stars():
+
+
+
+
+
+if __name__ == "__main__":
+    main()
