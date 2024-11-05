3b94ca6e3b61563c3d375df409e691fa31e4d4ce
Elena Moore <exm230000@utdallas.edu>
2024-11-04 18:05:51 -0600

    created stars in main function

--- src/ufosandstars.py
+++ src/ufosandstars.py
@@ -8,6 +8,19 @@ def main():
     screen.bgcolor("black")
     screen.setup(width=800, height=600)
 
+    stars = []
+    for _ in range(50):
+        star = turtle.Turtle()
+        star.hideturtle()
+        star.penup()
+        star.color("white")
+        star.goto(random.randint(-400, 400), random.randint(-300, 300))
+        star.dot(random.randint(2, 5))
+        stars.append(star)
+
+
+def twinkle():
+
 
 
 
