598a2739ab1b6df90e3cbd3760d213759e948051
Elena Moore <exm230000@utdallas.edu>
2024-11-16 14:17:11 -0600

    Added flashing lights and altered the beam to blink as it is being cast down

--- src/ufosandstars.py
+++ src/ufosandstars.py
@@ -33,21 +33,32 @@ def draw_stars():
         screen.blit(star[2], (star[0], star[1]))
 
 
-def draw_ufo(x, y, screen):
+def draw_lights(x, y, screen, frame_count):
+    positions = [(x + 20, y + 30), (x + 140, y + 30), (x + 80, y + 40)]
+    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    
+    # Change colors based on frame count
+    for i, pos in enumerate(positions):
+        color = colors[(frame_count // 10 + i) % len(colors)]
+        pygame.draw.circle(screen, color, pos, 5)
+
+def draw_ufo(x, y, screen, frame_count):
     pygame.draw.ellipse(screen, (150, 150, 150), (x, y, 160, 60))
     pygame.draw.ellipse(screen, (100, 100, 250), (x + 40, y - 20, 80, 40))
+    draw_lights(x, y, screen, frame_count)
 
-def animate_ufo(x, y, speed, beam_height, screen):
+def animate_ufo(x, y, speed, beam_height, screen, frame_count):
     global beam_active
     x += speed
-    draw_ufo(x, y, screen)
+    draw_ufo(x, y, screen, frame_count)
     if x > 800:
         beam_active = True
     if beam_active:
-        pygame.draw.polygon(screen, (255, 255, 100), 
-                            [(x + 80, y + 60), (x + 40, y + 60 + beam_height), 
-                             (x + 120, y + 60 + beam_height)])
-        beam_height += 5
+        if frame_count % 20 < 10:
+            pygame.draw.polygon(screen, (255, 255, 100), 
+                                [(x + 80, y + 60), (x + 30, y + 600 + beam_height), 
+                                 (x + 130, y + 600 + beam_height)])
+            beam_height += 5
     return x, beam_height
 
 
@@ -58,6 +69,7 @@ def main():
     global beam_active
     beam_active = False
     beam_height = 0
+    frame_count = 0
 
 
     running = True
@@ -70,10 +82,11 @@ def main():
 
         draw_stars()
 
-        ufo_x, beam_height = animate_ufo(ufo_x, ufo_y, ufo_speed, beam_height, screen)
+        ufo_x, beam_height = animate_ufo(ufo_x, ufo_y, ufo_speed, beam_height, screen, frame_count)
 
         pygame.display.flip()
         clock.tick(30)
+        frame_count += 1
 
     pygame.quit()
 
